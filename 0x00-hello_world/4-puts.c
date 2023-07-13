34b8863768a9d2c28e2834155f5bea4177859990
omoke664 <onokeleywes@gmail.com>
2023-07-13 12:48:55 -0700

    #include <stdio.h>

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- *main - main entry point
- *return  : always 0 (success)
- */
+*main - main entry point
+*return  : always 0 (success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
