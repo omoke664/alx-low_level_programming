6b2ff6044910ae4016859345c75d4033cd424481
omoke664 <omokeleywes@gmail.com>
2023-09-04 12:34:30 -0700

    hello

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
+	char *buf;
 	ssize_t fd;
 	ssize_t w;
 	ssize_t t;
@@ -18,11 +18,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	t = read(fd, buffer, letters);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
 
-	free(buffer);
+	free(buf);
 	close(fd);
 	return (w);
 }
